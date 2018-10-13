bc41791c9ba2ea1148b45e69dd39d925947bf9d1
Jordan Mack <jordan.mack@brilliantnotion.com>
2018-10-12 19:31:44 -0700

    Increase Dashboard refresh delay.

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,7 +1,7 @@
 const config =
 {
 	apiCacheExpiration: 60, // In minutes.
-	dashboardStatusRefreshInterval: 5000,
+	dashboardStatusRefreshInterval: 60 * 1000,
 	debouceDelayDefault: 200,
 	footerResizeTimerInterval: 2000,
 };
