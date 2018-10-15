518db4b241a1aaa6a09892f42c88f2168f11baf9
Jordan Mack <jordan.mack@brilliantnotion.com>
2018-10-15 01:13:40 -0700

    Add controls to Blocks page.

--- src/containers/Blocks/Blocks.jsx
+++ src/containers/Blocks/Blocks.jsx
@@ -1,22 +1,40 @@
+import iziToast from "izitoast";
+
 import Footer from "../../components/Footer/Footer.jsx";
 import Header from "../../components/Header/Header.jsx";
 import Navigation from "../../components/Navigation/Navigation.jsx";
 
 @observer class Component extends React.Component
 {
-	@observable data = [];
+	@observable data = {};
 	@observable showGenesisHash = false;
+	autorun = null;
 	refreshTimer = null;
+
+	constructor(props)
+	{
+		super(props);
+
+		this.controlType = React.createRef();
+		this.controlCount = React.createRef();
+		this.controlOffset = React.createRef();
+	}
 
 	componentDidMount()
 	{
-		this.clearTimer();
-		this.refreshData();
+		this.autorun = mobx.autorun(()=>
+		{
+			this.refreshData();
+		});
+
 		this.startTimer();
 	}
 
 	componentWillUnmount()
 	{
+		if(this.autorun)
+			this.autorun();
+
 		this.clearTimer();
 	}
 
@@ -29,11 +47,47 @@ import Navigation from "../../components/Navigation/Navigation.jsx";
 		}
 	};
 
+	handleControlChange = (e) =>
+	{
+		const params =
+		{
+			count: parseInt(this.controlCount.current.value),
+			offset: parseInt(store.routeParams.offset),
+			type: this.controlType.current.value,
+		};
+
+		router.navigate("blocks", params);
+	};
+
 	handleViewBlockClick = (e) =>
 	{
 		e.preventDefault();
 
 		router.navigate("block", {id: $(e.target).text()});
+	};
+
+	handleOffsetUpdate = (modifier) =>
+	{
+		const params =
+		{
+			count: parseInt(this.controlCount.current.value),
+			offset: parseInt(store.routeParams.offset),
+			type: this.controlType.current.value,
+		};
+		params.offset += modifier;
+
+		if(params.offset < 0)
+			params.offset = 0;
+
+		if(params.offset >= this.data.block_count)
+			params.offset = ((Math.floor(this.data.block_count / params.count) - 0) * params.count);
+
+		router.navigate("blocks", params);
+	};
+
+	isDataReady = () =>
+	{
+		return (this.data.hasOwnProperty("blocks") && this.data.hasOwnProperty("block_count"));
 	};
 
 	refreshClicked = action((e) =>
@@ -70,11 +124,37 @@ import Navigation from "../../components/Navigation/Navigation.jsx";
 	{
 		const _this = this;
 
-		api.getUrl(`/api/blocks/${store.routeParams.count}/${store.routeParams.offset}/1`, false)
+		if(store.route !== "blocks")
+			return false;
+
+		let block_type = ("type" in store.routeParams) ? store.routeParams.type : "all";
+
+		api.getUrl("/api/count", false)
 		.then(function(data)
 		{
-			_this.updateData(data);
+			const newData = _.merge(mobx.toJS(_this.data), data);
+			_this.updateData(newData);
+		})
+		.catch(function(error)
+		{
+			_this.updateData({});
 
+			log.error(error);
+			iziToast.error({title: "Error", message: "The specified block was not found."});
+		});
+
+		api.getUrl(`/api/blocks/${block_type}/${store.routeParams.count}/${store.routeParams.offset}/1`, false)
+		.then(function(data)
+		{
+			const newData = _.merge(mobx.toJS(_this.data), {blocks: null}, {blocks: data});
+			_this.updateData(newData);
+		})
+		.catch(function(error)
+		{
+			_this.updateData({});
+
+			log.error(error);
+			iziToast.error({title: "Error", message: "The specified block was not found."});
 		});
 	};
 
@@ -82,8 +162,14 @@ import Navigation from "../../components/Navigation/Navigation.jsx";
 	{
 		const _this = this;
 
+		if(!this.isDataReady())
+			return "Loading...";
+
+		if(this.data.blocks.length === 0)
+			return "No block data available.";
+
 		const tableRows = [];
-		this.data.forEach(function(row, r)
+		this.data.blocks.forEach(function(row, r)
 		{
 			const linkId = "#/block/" + row.id;
 			const linkHash = "#/block/" + row.hash;
@@ -117,7 +203,64 @@ import Navigation from "../../components/Navigation/Navigation.jsx";
     		        	{tableRows}
     		        </tbody>
 		        </table>
+		        {this.renderControls()}
 			</section>
+		);
+
+		return html;
+	};
+
+	renderControls = () =>
+	{
+		const block_type = ("type" in store.routeParams) ? store.routeParams.type : "all";
+		const block_count = this.data.block_count;
+		const count = store.routeParams.count;
+		const offset = store.routeParams.offset;
+
+		const pages = Math.ceil(block_count / count);
+		const page = Math.ceil(offset / count) + 1;
+
+		const disablePrevPageFast = (page <= 1);
+		const disablePrevPage = (page <= 1);
+		const disableNextPage = (page >= pages);
+		const disableNextPageFast = (page >= pages);
+
+		const html =
+		(
+			<div className="controls">
+				<div className="type control">
+					<label>
+						<span className="text">Block Type</span>
+						<select ref={this.controlType} value={block_type} onChange={this.handleControlChange}>
+							<option value="all">All</option>
+							<option value="file">File</option>
+							<option value="filehash">Filehash</option>
+							<option value="genesis">Genesis</option>
+							<option value="text">Text</option>
+							<option value="timestamp">Timestamp</option>
+						</select>
+					</label>
+				</div>
+				<div className="offset control">
+					<button className="prev-page-fast" onClick={()=>{this.handleOffsetUpdate(-10 * count);}} disabled={disablePrevPageFast}><i className="fas fa-angle-double-left"></i></button>
+					<button className="prev-page" onClick={()=>{this.handleOffsetUpdate(-1 * count);}} disabled={disablePrevPage}><i className="fas fa-angle-left"></i></button>
+					<span className="text">{page} of {pages}</span>
+					<button className="next-page" onClick={()=>{this.handleOffsetUpdate(1 * count);}} disabled={disableNextPage}><i className="fas fa-angle-right"></i></button>
+					<button className="next-page-fast" onClick={()=>{this.handleOffsetUpdate(10 * count);}} disabled={disableNextPageFast}><i className="fas fa-angle-double-right"></i></button>
+				</div>
+				<div className="count control">
+					<label>
+						<span className="text">Show</span>
+						<select ref={this.controlCount} value={count} onChange={this.handleControlChange}>
+							<option value="10">10</option>
+							<option value="25">25</option>
+							<option value="50">50</option>
+							<option value="75">75</option>
+							<option value="100">100</option>
+						</select>
+					</label>
+				</div>
+			</div>
 		);
 
 		return html;
