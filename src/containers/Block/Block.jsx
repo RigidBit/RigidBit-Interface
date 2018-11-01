ec916970a6fc3a026f177fa6e0b1725838ba1513
Jordan Mack <jordan.mack@brilliantnotion.com>
2018-11-01 13:31:57 -0700

    Add a second download link to the Block page to open in a new window.

--- src/containers/Block/Block.jsx
+++ src/containers/Block/Block.jsx
@@ -354,7 +354,7 @@ import Navigation from "../../components/Navigation/Navigation.jsx";
 				<tr key="download">
 					<td className={"download metric"}>Download:</td>
 					<td className={"download value"}>
-						<a href={api.apiUrlFromRelativePath("/api/file-download/"+data.id)}>Click to Download</a>
+						<a href={api.apiUrlFromRelativePath("/api/file-download/"+data.id)}>Download</a> <a href={api.apiUrlFromRelativePath("/api/file-inline/"+data.id)} target="_blank">Open in New Window</a>
 					</td>
 					<td className="empty" />
 				</tr>
