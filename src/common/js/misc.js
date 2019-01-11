eadb9ad2361b271e2307f82ae4776b443a3aba62
Jordan Mack <jordan.mack@brilliantnotion.com>
2019-01-10 22:01:05 -0800

    Move logo code to misc.js.

--- src/common/js/misc.js
+++ src/common/js/misc.js
@@ -256,3 +256,13 @@ export function isJson(string)
     }
     return true;
 }
+
+/**
+ * Returns the RigidBit logo in base64 encoding.
+ * 
+ * @return {String} A string containing the logo data in base64 encoding.
+ */
+export function rigidBitLogo()
+{
+	return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPwAAAAsCAMAAAB2SyV6AAAAKlBMVEUAAAB3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3dALpnnAAAADnRSTlMAQMCA4KBw0DAQIJBg/5WxDPAAAAFTSURBVGje7ZfRboQgEEUHtygi/f8PrYprV+wDNtlJJojG7Kbl3hczu+DMEZwr6pP29RUvfOgQKFfKsbBfs8dU3f7dx4WIiG72cM0VFSzAAx7wgC9GHzycHvFXI43tWdRMkkWRZC2GzzQ+28bECnQj/df0+/O7JHyI9ipbOLfnKb9mnwwPKFbwLcKP2PaABzzgAZ9rdSo+DCV2UP6g2kFqtlZKYmYW1swo/HZ+M9lLpX/9Jjxbk+PFiobl1xR8y8Ml4ZJDaijX3SW+F0JIuGvSpx8MxXfSkZakXNj2gAc84Eu3Oh/bYbV1/dt+J3+posPoeqs9PB/GDsjeRfg1MJ+3Cdt4h2J18wa/fRmMh9fGO2x7wAMe8GVb3XmpsxP1zI5qGUk0XZTrMnh3dmJdvyCJnAvvPOABD3jAlyB11X24C40LVh7wgAc84AH/v9QqJvsniv4B2wNQbtB0x5IAAAAASUVORK5CYII=";
+}
