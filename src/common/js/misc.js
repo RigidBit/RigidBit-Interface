cb6e61e191ebaa8de006c922375561967b75a072
Jordan Mack <jordan.mack@brilliantnotion.com>
2019-02-01 19:44:51 -0800

    Add file verification to Upload page.

--- src/common/js/misc.js
+++ src/common/js/misc.js
@@ -1,3 +1,4 @@
+import hash from "hash.js";
 import moment from "moment";
 
 /**
@@ -265,4 +266,63 @@ export function isJson(string)
 export function rigidBitLogo()
 {
 	return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPwAAAAsCAMAAAB2SyV6AAAAKlBMVEUAAAB3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3d3dALpnnAAAADnRSTlMAQMCA4KBw0DAQIJBg/5WxDPAAAAFTSURBVGje7ZfRboQgEEUHtygi/f8PrYprV+wDNtlJJojG7Kbl3hczu+DMEZwr6pP29RUvfOgQKFfKsbBfs8dU3f7dx4WIiG72cM0VFSzAAx7wgC9GHzycHvFXI43tWdRMkkWRZC2GzzQ+28bECnQj/df0+/O7JHyI9ipbOLfnKb9mnwwPKFbwLcKP2PaABzzgAZ9rdSo+DCV2UP6g2kFqtlZKYmYW1swo/HZ+M9lLpX/9Jjxbk+PFiobl1xR8y8Ml4ZJDaijX3SW+F0JIuGvSpx8MxXfSkZakXNj2gAc84Eu3Oh/bYbV1/dt+J3+posPoeqs9PB/GDsjeRfg1MJ+3Cdt4h2J18wa/fRmMh9fGO2x7wAMe8GVb3XmpsxP1zI5qGUk0XZTrMnh3dmJdvyCJnAvvPOABD3jAlyB11X24C40LVh7wgAc84AH/v9QqJvsniv4B2wNQbtB0x5IAAAAASUVORK5CYII=";
+}
+
+/**
+ * Creates a SHA256 hash from the file selected.
+ *
+ * Heavily modified from source: https://stackoverflow.com/a/28318964/9979
+ * 
+ * @param  {File} file A File object. Typically passed from an <input type="file">.
+ * @return {Promise} Returns a promise containing either the file hash on success, or the error on failure.
+ */
+export function hashFile(file)
+{
+	const promise = new Promise((resolve, reject) =>
+	{
+		const fileSize = file.size;
+		const chunkSize = 64 * 1024 * 1024;
+
+		let hasher = hash.sha256();
+		let offset = 0;
+
+		const errorHandler = function(e)
+		{
+			const error = _.get(e, "target.error.message", e);
+			reject(error);
+		};
+
+		const readHandler = function(e)
+		{
+			if(e.target.error === null)
+			{
+				hasher.update(e.target.result);
+				offset += e.target.result.length;
+			}
+			else
+				return errorHandler(e);
+
+			if(offset >= fileSize)
+				return resolve(hasher.digest("hex"));
+
+			// Read next chunk.
+			chunkReader(offset, chunkSize, file);
+		};
+
+		const chunkReader = function(offset, length, file)
+		{
+			const reader = new FileReader();
+			const blob = file.slice(offset, length + offset);
+
+			reader.onabort = errorHandler;
+			reader.onerror = errorHandler;
+			reader.onload = readHandler;
+			reader.readAsBinaryString(blob);
+		};
+
+		// Read the first chunk.
+		chunkReader(offset, chunkSize, file);
+	});
+
+	return promise;
 }
