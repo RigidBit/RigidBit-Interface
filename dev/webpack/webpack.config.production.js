f55002168375a9e2d5df1cda1ba1e4c2160d2d1b
Jordan Mack <jordan.mack@brilliantnotion.com>
2019-03-09 15:55:06 -0800

    Update dependencies.

--- dev/webpack/webpack.config.production.js
+++ dev/webpack/webpack.config.production.js
@@ -2,7 +2,7 @@ const config = require("./webpack.config.js")("prod");
 const { EnvironmentPlugin } = require("webpack");
 const { environmentPluginGenerator } = require("./environmentPluginGenerator.js");
 
-// Configure dev environment.
+// Configure environment.
 config.plugins.push(environmentPluginGenerator({stage: "prod"}));
 
 module.exports = config;
