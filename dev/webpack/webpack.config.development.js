92e1bb1baa80e4131b6b9cf2a204590e20a72657
Jordan Mack <jordan.mack@brilliantnotion.com>
2019-03-09 15:20:49 -0800

    Increase size limit warning in development.

--- dev/webpack/webpack.config.development.js
+++ dev/webpack/webpack.config.development.js
@@ -5,4 +5,7 @@ const { environmentPluginGenerator } = require("./environmentPluginGenerator.js"
 // Configure dev environment.
 config.plugins.push(environmentPluginGenerator({stage: "dev"}));
 
+// Increase the warning size of assets in development since it is uncompressed. 
+config.performance.maxAssetSize = 1024 * 1024 * 10;
+
 module.exports = config;
